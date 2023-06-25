537b6dc5fba699ff7507ce6b710d25f565f62328
xiong <xiongxiaowen0203@gmail.com>
2023-06-25 21:09:00 +0300

    add testing for display function

---
+++ Shortest_path/tests/test_display.py
@@ -0,0 +1,23 @@
+# test display_map function
+import unittest
+from flask import Flask, send_file
+from Shortest_path import app
+import random, string, os
+
+class TestDisplayMap(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_display_map(self):
+        cache_bust = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+        file_name = f"shortest_path_{cache_bust}.html"
+        file_path = os.path.join(os.getcwd(), 'templates', file_name)
+        response = self.app.get(f'/display_map/{file_path}')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, 'application/octet-stream')
+        self.assertEqual(response.headers['Content-Disposition'], f'attachment; filename={file_path}')
+
+if __name__ == '__main__':
+    unittest.main()
