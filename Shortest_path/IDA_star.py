a090bafd746271aaaf9a60c883ec4ef8e2e5ab0b
xiong <xiongxiaowen0203@gmail.com>
2023-06-28 22:45:03 +0300

    heuristic and search function as seperate class

--- Shortest_path/IDA_star.py
+++ Shortest_path/IDA_star.py
@@ -1,13 +1,44 @@
-#implementing IDA*
+#this file for implementing IDA*
 
+# heuristic function (Euclidean distance between two points)
+# estimates the distance between a given node and the goal node
+def heuristic(start_coordinates, end_coordinates):
+    x1, y1 = start_coordinates
+    x2, y2 = end_coordinates
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+# Define the IDA* search function
+def search(current_coordinates, g, bound, end_coordinates):
+    f = g + heuristic(current_coordinates, end_coordinates)
+    if f > bound:
+        return f
+
+    if current_coordinates == end_coordinates:
+        return "FOUND"
+
+    min_cost = float("inf")
+
+    # neighbors sorted by the combined cost of the path from the start node and the heuristic estimate
+    neighbors = sorted(graph.neighbors(current_coordinates), key=lambda neighbor: g + graph[current_coordinates][neighbor]['weight'] + heuristic(neighbor, end_coordinates))
+
+    # Explore the neighbors of the current node
+    for neighbor in neighbors:
+        # Calculate the distance from the start to the neighbor through the current node
+        distance = g + graph[current_coordinates][neighbor]['weight']
+        if distance < distances[neighbor]:
+            distances[neighbor] = distance
+            visited[neighbor] = current_coordinates
+
+            cost = search(neighbor, distance, bound)
+            if cost == "FOUND":
+                return "FOUND"
+            if cost < min_cost:
+                min_cost = cost
+
+    return min_cost
+
+#Define the ida_star function
 def ida_star(graph, start_coordinates, end_coordinates):
-    # heuristic function (Euclidean distance between two points)
-    # estimates the distance between a given node and the goal node
-    def heuristic(start_coordinates, end_coordinates):
-        x1, y1 = start_coordinates
-        x2, y2 = end_coordinates
-        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-
     distances = {}
     for node in graph.nodes:
         distances[node] = float("inf")
@@ -15,37 +46,6 @@ def ida_star(graph, start_coordinates, end_coordinates):
 
     # for visited nodes
     visited = {}
-
-    # Define the IDA* search function
-    def search(current_coordinates, g, bound):
-        f = g + heuristic(current_coordinates, end_coordinates)
-        if f > bound:
-            return f
-
-        if current_coordinates == end_coordinates:
-            return "FOUND"
-
-        min_cost = float("inf")
-
-      # neighbors sorted by the combined cost of the path from the start node and the heuristic estimate
-        neighbors = sorted(graph.neighbors(current_coordinates), key=lambda neighbor: g + graph[current_coordinates][neighbor]['weight'] + heuristic(neighbor, end_coordinates))
-
-
-        # Explore the neighbors of the current node
-        for neighbor in neighbors:
-            # Calculate the distance from the start to the neighbor through the current node
-            distance = g + graph[current_coordinates][neighbor]['weight']
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                visited[neighbor] = current_coordinates
-
-                cost = search(neighbor, distance, bound)
-                if cost == "FOUND":
-                    return "FOUND"
-                if cost < min_cost:
-                    min_cost = cost
-
-        return min_cost
 
     # Perform the IDA* search
     bound = heuristic(start_coordinates, end_coordinates)
@@ -55,7 +55,6 @@ def ida_star(graph, start_coordinates, end_coordinates):
             break
         if cost == float("inf"):
             return []
-
         bound = cost
 
     # Reconstruct the shortest path
