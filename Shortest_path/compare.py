62c9c8d9d3230de70b9d2729e1e95cb8a7376d05
xiong <xiongxiaowen0203@gmail.com>
2023-06-30 00:18:19 +0300

    print execution time

--- Shortest_path/compare.py
+++ Shortest_path/compare.py
@@ -1,5 +1,5 @@
 """
-This module is not part of the program.
+This module is not part of the program, but for checking performance.
 Use this verion to compare the execution times of geocoding, IDA* algorithm and Dijkstra's algorithm.
 it tells the fastest algorithm.
 """
@@ -22,8 +22,15 @@ graph = networkx.Graph()
 # use the start_coordinates and end_coordinates to calculate the shortest path
 # Return the shortest path as a list of coordinates
 def find_shortest_path(start_coordinates, end_coordinates):
-    print("Start Coordinates after geocoding:", start_coordinates)
-    print("End Coordinates after geocoding:", end_coordinates)
+
+    start_coordinates = geocode(start_point)
+    end_coordinates = geocode(end_point)
+
+    # Add the start and end nodes with their coordinates to the graph
+    graph.add_node('start', pos=start_coordinates)
+    graph.add_node('node1', pos=(0, 0))  # Add 'node1' with placeholder coordinates
+    graph.add_node('node2', pos=(0, 0))  # Add 'node2' with placeholder coordinates
+    graph.add_node('end', pos=end_coordinates)
 
     # Define the distance function (Euclidean distance between two points)
     def distance(point1, point2):
@@ -35,6 +42,7 @@ def find_shortest_path(start_coordinates, end_coordinates):
     def calculate_distance(node1, node2):
         return distance(graph.nodes[node1]['pos'], graph.nodes[node2]['pos'])
 
+
     # Add edges to the graph connecting neighboring nodes
     graph.add_edge('start', 'node1', weight=calculate_distance('start', 'node1'))
     graph.add_edge('node1', 'node2', weight=calculate_distance('node1', 'node2'))
@@ -43,7 +51,7 @@ def find_shortest_path(start_coordinates, end_coordinates):
     print("Graph Edges:", graph.edges)
     print("Graph Nodes:", graph.nodes)
 
-        # Define a tolerance for coordinate matching
+    # Define a tolerance for coordinate matching
     tolerance = 0.00001
 
     # Use Algorithm:
@@ -52,16 +60,25 @@ def find_shortest_path(start_coordinates, end_coordinates):
         # Handle the case when the coordinates are not present in the graph
         print('Invalid start or end coordinates. Please provide valid coordinates.')
         return None, None
-    else:
-        # if use ida_star function (IDA* Algorithm) to find the shortest path, run below line: 
-        ida_star_path = ida_star(graph, start_coordinates, end_coordinates)
-        print('IDA* Path:', ida_star_path)
-        # if use Dijkstra’s Algorithm to find the shortest path, run below line: 
-        dijkstra_path = dijkstra(graph, 'start', 'end')
-        print('Dijkstra Path:', dijkstra_path)
 
-    return ida_star_path, dijkstra_path
+    # if use ida_star function (IDA* Algorithm) to find the shortest path, run below line: 
+    ida_star_path = ida_star(graph, start_coordinates, end_coordinates)
+    # if use Dijkstra’s Algorithm to find the shortest path, run below line: 
+    dijkstra_path = dijkstra(graph, 'start', 'end')
 
+    if not dijkstra_path:
+        print("dijkstra_path no found")
+
+    if not ida_star_path:
+        print("ida_star_path no found")
+
+    # Retrieve the coordinates of the nodes in the path
+    path_coordinatesida_star = [graph.nodes[node]['pos'] for node in ida_star_path]
+    path_coordinatesdijkstra = [graph.nodes[node]['pos'] for node in dijkstra_path]
+
+    #print the found path
+    print('IDA* Path:', path_coordinatesida_star)
+    print('Dijkstra Path:', path_coordinatesdijkstra)
 
 
 def compare_shortest_paths(start_coordinates, end_coordinates):
@@ -105,6 +122,7 @@ def geocode(address):
         print(f"Geocoding failed for address '{address}': {ex}")
     return None, execution_time
 
+
           
 if __name__ == '__main__':
     start_point = input("Enter the start address: ")
@@ -113,14 +131,9 @@ if __name__ == '__main__':
     start_coordinates, start_execution_time = geocode(start_point)
     end_coordinates, end_execution_time = geocode(end_point)
 
-    if start_coordinates is not None and end_coordinates is not None:
-        # Add the start and end nodes with their coordinates to the graph
-        graph.add_node('start', pos=start_coordinates)
-        graph.add_node('node1', pos=(0, 0))  # Add 'node1' with placeholder coordinates
-        graph.add_node('node2', pos=(0, 0))  # Add 'node2' with placeholder coordinates
-        graph.add_node('end', pos=end_coordinates)
+    #print geocoding coordinates & execution time
+    print("Start Coordinates after geocoding:", start_coordinates)
+    print("End Coordinates after geocoding:", end_coordinates)
+    print("Geocoding Execution Time (Start Address):", start_execution_time)
+    print("Geocoding Execution Time (End Address):", end_execution_time)
 
-        compare_shortest_paths(start_coordinates, end_coordinates)
-        #print geocoding execution time
-        print("Geocoding Execution Time (Start Address):", start_execution_time)
-        print("Geocoding Execution Time (End Address):", end_execution_time)
